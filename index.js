dbe5179f2d0429bf4ccc37590e976437f9272d63
Jared Grippe <jared@deadlyicon.com>
2016-10-05 16:17:16 -0700

    release 0.0.3

--- index.js
+++ index.js
@@ -3,11 +3,30 @@ import querystring from 'querystring'
 import PathnameRouter from 'pathname-router'
 
 export default class SimpleReactRouter extends Component {
+
+  static childContextTypes = {
+    location:       React.PropTypes.object.isRequired,
+    route:          React.PropTypes.object.isRequired,
+    redirectTo:     React.PropTypes.func,
+    locationToHref: React.PropTypes.func,
+  }
+
   constructor(props){
     super()
-    this.popstate = this.popstate.bind(this)
+    this.rerender = this.rerender.bind(this)
     this.redirectTo = this.redirectTo.bind(this)
+    this.locationToHref = this.locationToHref.bind(this)
     this.update()
+    addEventListener('popstate', this.rerender)
+  }
+
+  componentWillUnmount(){
+    removeEventListener('popstate', this.rerender)
+  }
+
+  rerender(event){
+    this.update()
+    this.forceUpdate()
   }
 
   update() {
@@ -32,66 +51,48 @@ export default class SimpleReactRouter extends Component {
     )
   }
 
-  componentDidMount(){
-    // bind to push state event
-    addEventListener('popstate', this.popstate)
-  }
-
-  componentWillUnmount(){
-    removeEventListener('popstate', this.popstate)
-  }
-
-  popstate(event){
-    this.update()
-  }
-
-  static childContextTypes = {
-    route: React.PropTypes.object,
-    redirectTo: React.PropTypes.func,
-  }
-
-  getChildContext(){
+  getChildContext() {
     return {
+      location: this.location,
       route: this.route,
       redirectTo: this.redirectTo,
+      locationToHref: this.locationToHref,
     }
   }
 
-  redirectTo(path, query){
+  redirectTo(href, replace){
+    href = this.locationToHref(href)
+    if (replace){
+      history.replaceState(null, document.title, href)
+    }else{
+      history.pushState(null, document.title, href)
+    }
+    this.rerender()
+  }
 
+  locationToHref(location) {
+    if (!location || typeof location === 'string') return location
+    let href = location.pathname || this.location.pathname
+    let query = location.query
+    if (typeof query === 'object') query = objectToSearch(query)
+    if (query) href += '?'+query
+    return href
   }
 
   render(){
     if (!this.route){
       return React.createElement('span', null, 'No Route Found')
     }
-    const { Component } = this.route.params
-    return React.createElement(Component, {})
+    const { params, location } = this.route
+    const { Component } = params
+    const props = Object.assign({}, this.props, { params, location })
+    return React.createElement(Component, props)
   }
 }
 
 const searchToObject = (search) => {
   return querystring.parse((search || '').replace(/^\?/, ''))
 }
-
-
-// const objectToSearch = (params) => {
-//   var search = objectToQueryString(params);
-//   return search.length === 0 ? '' : '?'+search;
-// }
-
-// const objectToQueryString = (params) => {
-//   if (!params) return;
-//   let pairs = []
-//   Object.keys(params).forEach( key => {
-//     let value = params[key]
-//     if (value === true){
-//       return pairs.push(encodeURIComponent(key));
-//     }
-//     if (value === false || value === null || value === undefined){
-//       return;
-//     }
-//     pairs.push(encodeURIComponent(key)+'='+encodeURIComponent(value));
-//   });
-//   return pairs.join('&');
-// }
+const objectToSearch = (object) => {
+  return querystring.stringify(object)
+}
